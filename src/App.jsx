0313dce41e55e4cba12c6c85f7c158e24f7f8cd2
mnabilfs <mnabilfs22@gmail.com>
2025-04-09 00:21:47 +0700

    Add NotFoundPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import NavbarFooter from "./components/NavbarFooter";
 import Home from "./pages/Home";
 import Rating from "./pages/Rating";
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          {/* Include NavbarFooter at Route */}
           <Route path="/" element={<NavbarFooter />} >
           <Route index element={<Home />} />
           <Route path="rating" element={<Rating/>} />
           </Route>
+          {/* NotFound */}
+          <Route path="*" element={<NotFoundPage/>}/>
         </Routes>
       </BrowserRouter>
     </>
